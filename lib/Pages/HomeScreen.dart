88d70cd75a82fd59c8ccaab1f15a36e165b8b617
KReTeR14 <kushtejani50@gmail.com>
2024-01-21 17:34:14 +0530

    Commit 1

--- lib/Pages/HomeScreen.dart
+++ lib/Pages/HomeScreen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+    // final TextEditingController _userNameTextController = TextEditingController();
 
     String username= 'kush';
   @override
