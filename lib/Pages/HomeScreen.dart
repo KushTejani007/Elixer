67bc968deb76cded065af739321d44f428ef88ae
KReTeR14 <kushtejani50@gmail.com>
2024-01-30 18:38:42 +0530

    BottomNav

--- lib/Pages/HomeScreen.dart
+++ lib/Pages/HomeScreen.dart
@@ -1,120 +1,163 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/ListPage.dart';
 import 'package:flutter_application_1/Pages/Wallet.dart';
-// import 'package:flutter_application_1/Pages/Login.dart';
-// import 'package:flutter_application_1/Pages/Reuseable.dart';
+import 'package:flutter_application_1/Pages/menu.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-// import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-    // final TextEditingController _userNameTextController = TextEditingController();
-// commit 2 test
-    String username= 'kush';
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(backgroundColor: const Color.fromARGB(255, 20, 20, 20),
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(115), // Set the height here
-        child: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0, // Remove shadow
-    // title: Text('Your Title'),
-          centerTitle: true,
-          bottom: PreferredSize(
-      preferredSize: const Size.fromHeight(0), // Hide the existing bottom
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-             width: 300,
-              height: 70,
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.teal,                      
-              ),
-                            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(115), // Set the height here
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0, // Remove shadow
+            centerTitle: true,
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(0), // Hide the existing bottom
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(onPressed: () {},style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all<Size>(const Size(130, 60)), 
-                    backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 20, 20, 20)), // Set the background color
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder( borderRadius: BorderRadius.circular(20))
-                    )
-                  ), child:const Text('Maps'),),
-
-                  ElevatedButton(onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const ListPage()), );
-
-                  },style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all<Size>(const Size(130, 60)),
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.teal),                     // Set the background color
-                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder( borderRadius: BorderRadius.circular(20))
-                    )
-                  ), child:const Text('List'),),
-
+                  Container(
+                    width: 300,
+                    height: 70,
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.teal,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            fixedSize: MaterialStateProperty.all<Size>(
+                                const Size(130, 60)),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(
+                                    const Color.fromARGB(255, 20, 20, 20)),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                          child: const Text('Maps'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const ListPage(),
+                              ),
+                            );
+                          },
+                          style: ButtonStyle(
+                            fixedSize: MaterialStateProperty.all<Size>(
+                                const Size(130, 60)),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.teal),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                          child: const Text('List'),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-// Example color, replace with your desired color
+            ),
           ),
-        ],
-      ),
-    ),
-  ),
-),
-      
-      bottomNavigationBar:const GNav(
-          tabBackgroundColor: Color.fromARGB(250, 20, 20, 20),
+        ),
+        bottomNavigationBar: GNav(
+          selectedIndex: _selectedIndex,
+          onTabChange: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+            _navigateToPage(index);
+          },
+          tabBackgroundColor: const Color.fromARGB(250, 20, 20, 20),
           tabBorderRadius: 20,
-          tabMargin: EdgeInsets.all(10),
+          tabMargin: const EdgeInsets.all(10),
           backgroundColor: Colors.teal,
           activeColor: Colors.white,
           color: Colors.white,
           gap: 5,
-        tabs:[
-          GButton(
-          
-          icon: Icons.home,
-          // iconColor: Colors.white,
-          iconSize: 25,
-          text: 'Home',),
-          GButton(
-          icon: Icons.wallet,
-          // iconColor: Colors.white,
-          iconSize: 25,
-          text: 'Wallet',
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              iconSize: 25,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.wallet,
+              iconSize: 25,
+              text: 'Wallet',
+            ),
+            GButton(
+              icon: Icons.menu,
+              iconSize: 25,
+              text: 'Menu',
+            ),
+          ],
         ),
-          GButton(
-          icon: Icons.menu,
-          // iconColor: Colors.white,
-          iconSize: 25,
-          text: 'Menu',)]) ,
-        
-      
       ),
     );
   }
-  void _navigateToWalletPage() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => WalletPage()),
-    );
-  }
 
+  void _navigateToPage(int index) {
+    switch (index) {
+      case 0:
+        // Navigate to Home page
+        // If Home page is another widget, replace MaterialPageRoute with your desired route.
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+        // );
+        break;
+      case 1:
+        // Navigate to WalletPage
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const WalletPage()),
+        );
+        break;
+      case 2:
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MenuPage()),
+        );
+        // Navigate to Menu page
+        // Add navigation for MenuPage here
+        break;
+      default:
+        break;
+    }
+  }
 }
--- lib/Pages/Wallet.dart
+++ lib/Pages/Wallet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Pages/HomeScreen.dart';
+import 'package:flutter_application_1/Pages/menu.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 
 class WalletPage extends StatefulWidget {
@@ -9,11 +12,90 @@ class WalletPage extends StatefulWidget {
 }
 
 class _WalletPageState extends State<WalletPage> {
+  int _selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(backgroundColor: Color.fromARGB(255, 20, 20, 20),),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+        // extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          toolbarHeight: 100,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text('Wallet'),
+        backgroundColor: Colors.teal,
+        
+      ),
+
+
+      bottomNavigationBar: GNav(
+          selectedIndex: _selectedIndex,
+          onTabChange: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+            _navigateToPage(index);
+          },
+          tabBackgroundColor: const Color.fromARGB(250, 20, 20, 20),
+          tabBorderRadius: 20,
+          tabMargin: const EdgeInsets.all(10),
+          backgroundColor: Colors.teal,
+          activeColor: Colors.white,
+          color: Colors.white,
+          gap: 5,
+          tabs: const [
+            GButton(
+              icon: Icons.home,
+              iconSize: 25,
+              text: 'Home',
+            ),
+            GButton(
+              icon: Icons.wallet,
+              iconSize: 25,
+              text: 'Wallet',
+            ),
+            GButton(
+              icon: Icons.menu,
+              iconSize: 25,
+              text: 'Menu',
+            ),
+          ],
+        ),
+
       
+      ),
     );
+  }
+  void _navigateToPage(int index) {
+    switch (index) {
+      case 0:
+        // Navigate to Home page
+        // If Home page is another widget, replace MaterialPageRoute with your desired route.
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomeScreen()),
+        );
+        break;
+      case 1:
+        // Navigate to WalletPage
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => const WalletPage()),
+        // );
+        break;
+      case 2:
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MenuPage()),
+        );
+        // Navigate to Menu page
+        // Add navigation for MenuPage here
+        break;
+      default:
+        break;
+    }
   }
 }
