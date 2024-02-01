eac17940144d955a49166f2a6969e458e3495ba6
KReTeR14 <kushtejani50@gmail.com>
2024-02-01 19:11:31 +0530

    Route

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -1,14 +1,8 @@
-// ignore_for_file: prefer_const_constructors
-
-// import "dart:js_interop_unsafe";
-
-import "package:firebase_auth/firebase_auth.dart";
-// import "package:firebase_core/firebase_core.dart";
-// import "package:firebase_core/firebase_core.dart";
-import "package:flutter/material.dart";
-import "package:flutter_application_1/Pages/HomeScreen.dart";
-import "package:flutter_application_1/Pages/Reuseable.dart";
-import "package:flutter_application_1/Pages/SignUpScreen.dart";
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Pages/HomeScreen.dart';
+import 'package:flutter_application_1/Pages/SignUpScreen.dart';
+import 'package:flutter_application_1/Pages/Reuseable.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -18,54 +12,64 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
-  
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    // Check if user is already signed in
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user != null) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+          (route) => false,
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body:Container(
+      body: Container(
         padding: EdgeInsets.all(30),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-
-      color: Color.fromARGB(255, 20, 20, 20),
-      child: Column(
-        children: <Widget>[
-          logowidget("assets/images/Logo1.png"),
-          // const SizedBox(
-          //         height: 5,
-          //       ),
-                reusableTextField("Enter UserName", Icons.person_outline, false,
-                    _emailTextController),
-                const SizedBox(
-                  height: 15,
-                ),
-                reusableTextField("Enter Password", Icons.lock_outline, true,
-                    _passwordTextController),
-                const SizedBox(
-                  height: 15,
-                ),
-                firebaseUIButton(context, 'Login', (){
-                  FirebaseAuth.instance
-                      .signInWithEmailAndPassword(
-                          email: _emailTextController.text,
-                          password: _passwordTextController.text)
-                      .then((value) {
-                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => HomeScreen()), (route) => false);
-                  }).onError((error, stackTrace) {
-                    print("Error ${error.toString()}");
-                  });
-                }),
-                signUpOption()
-        ],
-      )      
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Color.fromARGB(255, 20, 20, 20),
+        child: Column(
+          children: <Widget>[
+            logowidget("assets/images/Logo1.png"),
+            reusableTextField("Enter UserName", Icons.person_outline, false,
+                _emailTextController),
+            const SizedBox(height: 15),
+            reusableTextField("Enter Password", Icons.lock_outline, true,
+                _passwordTextController),
+            const SizedBox(height: 15),
+            firebaseUIButton(context, 'Login', () {
+              FirebaseAuth.instance
+                  .signInWithEmailAndPassword(
+                      email: _emailTextController.text,
+                      password: _passwordTextController.text)
+                  .then((value) {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                    (route) => false);
+              }).onError((error, stackTrace) {
+                print("Error ${error.toString()}");
+              });
+            }),
+            signUpOption(),
+          ],
+        ),
       ),
-
     );
   }
-   Row signUpOption() {
+
+  Row signUpOption() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -78,13 +82,11 @@ class _SignInScreenState extends State<SignInScreen> {
           },
           child: const Text(
             " Sign Up",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold),
           ),
         )
       ],
     );
   }
-   
-
-
 }
